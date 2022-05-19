463cc6afdec532368e879a0f4bfd23ce5d1507cf
jecsham <jl4rvideos@gmail.com>
2022-05-19 18:15:09 -0500

    remove hardward word

--- src/components/HardwareTable.tsx
+++ src/components/HardwareTable.tsx
@@ -6,12 +6,12 @@ interface HardwareTableProps {
 function HardwareTable({ data }: HardwareTableProps) {
   return (
     <table className="uk-table uk-table-divider uk-table-small uk-table-middle uk-margin-remove-top">
-      <thead>
+      {/* <thead>
         <tr>
           <th></th>
           <th className="uk-table-expand">Hardware</th>
         </tr>
-      </thead>
+      </thead> */}
       <tbody id="table-body">
         {data.map((e, index) => (
           <HardwareTableRow key={index} title={e.title} content={e.content} />
