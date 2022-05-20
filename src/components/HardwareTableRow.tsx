ebeea34a603aa5495f88b5bfd3b0dc8b6b243f8d
jecsham <jl4rvideos@gmail.com>
2022-05-19 20:42:18 -0500

    handling not available data

--- src/components/HardwareTableRow.tsx
+++ src/components/HardwareTableRow.tsx
@@ -3,15 +3,24 @@ interface HardwareTableRowProps {
   content: string[];
 }
 function HardwareTableRow({ title, content }: HardwareTableRowProps) {
+  const contentValue = (content: string[]) => {
+    if (content?.length === 0) {
+      return <li key="0">No data available</li>;
+    }
+    return (
+      <>
+        {content.map((e, index) => (
+          <li key={index}>{e}</li>
+        ))}
+      </>
+    );
+  };
+
   return (
     <tr>
       <td className="uk-text-small uk-text-bold ">{title}</td>
       <td className="uk-text-small uk-padding-remove-right">
-        <ul className="uk-list uk-list-divider">
-          {content.map((e, index) => (
-            <li key={index}>{e}</li>
-          ))}
-        </ul>
+        <ul className="uk-list uk-list-divider">{contentValue(content)}</ul>
       </td>
     </tr>
   );
